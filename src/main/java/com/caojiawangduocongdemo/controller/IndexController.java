4c623a3bc29ea631557021bd7f1ff4f02f6dda14
caojia <18894037898@163.com>
2019-10-23 21:32:01 +0800

    作者提交

--- src/main/java/com/caojiawangduocongdemo/controller/IndexController.java
+++ src/main/java/com/caojiawangduocongdemo/controller/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author ${caojia}
+ * @author caojia
  * @time 下午 7:52
  */
 @RestController
