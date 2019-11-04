46f762fa14226dddc938728513f1ad95825eda52
woshicaojia <18894037898@163.com>
2019-11-04 16:17:30 +0800

    配置文件修改提交

---
+++ src/main/java/com/caojiawangduocongdemo/controller/login/LoginController.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2019, 万维公司 All rights reserved.
+ * 万维公司 专有/保密源代码,未经许可禁止任何人通过任何渠道使用、修改源代码.
+ */
+package com.caojiawangduocongdemo.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author caojia
+ * @createtime 2019-11-04 15:36
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+}
