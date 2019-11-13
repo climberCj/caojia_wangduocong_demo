faa95947fa2bf501d874435a2b70aadeab00e82a
caojia <18894037898@163.com>
2019-11-13 22:26:11 +0800

    主类提交

--- src/main/java/com/caojiawangduocongdemo/CaojiaWangduocongDemoApplication.java
+++ src/main/java/com/caojiawangduocongdemo/CaojiaWangduocongDemoApplication.java
@@ -1,9 +1,11 @@
 package com.caojiawangduocongdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.caojiawangduocongdemo.dao")
 public class CaojiaWangduocongDemoApplication {
 
     public static void main(String[] args) {
