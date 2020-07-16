7c744a9a4d2f64054dc0d470f8ac072b219da372
woshicaojia <2782019153@qq.com>
2020-07-16 17:56:18 +0800

    测试类提交

--- src/test/java/com/caojiawangduocongdemo/UserServiceImplTest.java
+++ src/test/java/com/caojiawangduocongdemo/UserServiceImplTest.java
@@ -1,11 +1,15 @@
 package com.caojiawangduocongdemo;
 
+import com.caojiawangduocongdemo.config.redis.RedisUtils;
 import com.caojiawangduocongdemo.entity.Permission;
 import com.caojiawangduocongdemo.entity.Role;
 import com.caojiawangduocongdemo.entity.User;
 import com.caojiawangduocongdemo.service.custom.UserService;
+import org.apache.shiro.crypto.hash.Md5Hash;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,8 +23,11 @@ import java.util.Set;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceImplTest {
+    Logger logger = LoggerFactory.getLogger(UserServiceImplTest.class);
     @Autowired
     private UserService userService;
+    @Autowired
+    private RedisUtils redisUtils;
 
     @Test
     public void findByUsername(){
@@ -33,5 +40,29 @@ public class UserServiceImplTest {
             }
             System.out.println(role.getRoleName());
         }
+    }
+
+    @Test
+    public void getCache(){
+        //查询缓存中是否存在
+        boolean hasKey = redisUtils.exists("caojia");
+        User user = null;
+        if(hasKey){
+            //从缓存中获取数据
+            Object value = redisUtils.get("caojia");
+            logger.info("缓存中的数据："+value);
+        }else{
+            logger.info("从数据库中获取数据");
+            user = userService.findByUsername("caojia");
+            //存入缓存
+            redisUtils.set("caojia",user);
+            logger.info("数据插入缓存："+user);
+        }
+    }
+
+    @Test
+    public void getPass(){
+        String pass = new Md5Hash(new Md5Hash("123456","heis")).toHex();
+        System.out.println(pass);
     }
 }
